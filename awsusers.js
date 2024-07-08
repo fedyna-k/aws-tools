2b26325d872f0d3b668719cf38c16e2259fe0f8f
Kevin FEDYNA <kevin.fedyna@thalescloud.io>
2024-07-08 15:28:59 +0200

    Added filters

---
+++ awsusers.js
@@ -0,0 +1,175 @@
+/**
+ * @file Asynchronous user fetcher.
+ * @version 1.0.0
+ * @author Kevin Fedyna
+ */
+
+const { IAMClient, ListUsersCommand, ListGroupsForUserCommand } = require("@aws-sdk/client-iam");
+
+/**
+ * Prints log with custom tag.
+ * @param  {...any} data The data to log.
+ */
+function log(...data) {
+    console.log("\x1b[1m\x1b[93m[users.js]\x1b[0m  ", ...data);
+}
+
+/**
+ * Prints progress bar.
+ * @param  {number} percentage The percentage of progress from 0 to 1.
+ */
+function progress(percentage) {
+    let full = Math.floor(percentage * 50);
+    process.stdout.write(`\r\x1b[1m\x1b[93m[group audit]\x1b[0m  [${"#".repeat(full)}${".".repeat(50 - full)}] (${(percentage * 100).toFixed(1)}%)`);
+
+    if (percentage == 1) {
+        console.log("");
+    }
+}
+
+/**
+ * Get all AWS users.
+ * @returns {string[]} The list of all users.
+ */
+async function getUsers() {
+    log("Requesting users...");
+    let response;
+    let users = [];
+    let marker = undefined;
+
+    do {
+        const input = { Marker: marker };
+        const command = new ListUsersCommand(input);
+        response = await client.send(command);
+
+        const usernames = response.Users.map(user => user.UserName);
+        users = users.concat(usernames);
+
+        marker = response.Marker;
+    } while (response.IsTruncated);
+
+    log(`${users.length} users found.`);
+    log("Requesting users [OK]");
+    return users;
+}
+
+/**
+ * Get all user groups asynchronously.
+ * @param {string[]} users The list of all users.
+ * @returns {string[][]} The list of all groups 
+ */
+async function getUsersGroups(users) {
+    log("Retreiving users groups...");
+
+    const requestUserGroup = async user => {
+        const input = { UserName: user };
+        const command = new ListGroupsForUserCommand(input);
+        return client.send(command);
+    };
+
+    let groups = [];
+    let usersCopy = [...users];
+    
+    // Send batch of size MAX_SOCKET
+    while (usersCopy.length) {
+        const responses = await Promise.all(usersCopy.splice(0, MAX_SOCKET).map(requestUserGroup));
+        const responsesGroups = responses.map(response => response.Groups.map(group => group.GroupName));
+        groups = groups.concat(responsesGroups);
+
+        progress(groups.length / users.length);
+    }
+
+    log("Retreiving users groups [OK]");
+    return groups;
+}
+
+/**
+ * Filter users according to given users filters.
+ * @param {string[]} users The user list.
+ * @param {string[]} filters the filter list (regex).
+ * @returns The filtered users.
+ */
+function filterUsers(users, filters) {
+    const regexFilters = filters.map(filter => new RegExp(filter));
+    return users.filter(user =>
+        regexFilters.reduce((matching, filter) => user.match(filter) || matching)
+        , false);
+}
+
+/**
+ * Filter users according to given groups filters.
+ * @param {string[]} users The user list.
+ * @param {string[][]} groups The groups associated to users.
+ * @param {string[]} filters the filter list (regex).
+ * @returns The filtered users.
+ */
+function filterGroups(users, groups, filters) {
+    const regexFilters = filters.map(filter => new RegExp(filter));
+    return users.filter((user, index) =>
+        groups[index].reduce((matchingGroup, group) =>
+            regexFilters.reduce((matching, filter) => group.match(filter) || matching) || matchingGroup
+            , false)
+        , false);
+} 
+
+/**
+ * Read the CLI args and return help message if unable to parse.
+ * @param {string[]} args The CLI args.
+ * @returns {[string[], string[]]} The user and group filters.
+ */
+function readArgs(args) {
+    const help = () => {
+        log("Usage: node awsusers.js [-u <...usernames>] [-g <...groups>]");
+        log("Arguments:");
+        log("   usernames: regex matching user names.");
+        log("   group:     regex matching group names.");
+        process.exit(0);
+    }
+
+    let userFilters = [];
+    let groupFilters = [];
+    let inUser = false;
+    let inGroup = false;
+
+    for (let arg of args) {
+        if (arg == "-u") {
+            inUser = true;
+            inGroup = false;
+        } else if (arg == "-g") {
+            inGroup = true;
+            inUser = false;
+        } else if (inUser) {
+            userFilters.push(arg);
+        } else if (inGroup) {
+            groupFilters.push(arg);
+        } else {
+            help();
+        }
+    }
+
+    if (inUser && userFilters.length == 0 || inGroup && groupFilters.length == 0) {
+        help();
+    }
+
+    return [ userFilters, groupFilters ];
+}
+
+/**
+ * Main function to run.
+ * @param {string[]} args The CLI args. 
+ */
+async function main(args) {
+    const [ userFilters, groupFilters ] = readArgs(args);
+
+    const users = await getUsers();
+    const filteredUsers = userFilters.length ? filterUsers(users, userFilters) : users;
+    
+    const groups = await getUsersGroups(filteredUsers);
+    const filteredGroups = groupFilters.length ? filterGroups(filteredUsers, groups, groupFilters) : groups;
+    
+    console.log(filteredGroups);
+}
+
+const MAX_SOCKET = 15;  // Throttling exceptions after this number
+const client = new IAMClient();
+main(process.argv.slice(2));
