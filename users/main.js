71e32cc133da8acadda4aa28e2f17f0eadeaf09f
Kevin FEDYNA <kevin.fedyna@thalescloud.io>
2024-07-08 12:27:18 +0200

    test for getting users

--- users/main.js
+++ users/main.js
@@ -36,39 +36,30 @@ function progress(percentage) {
 async function getUsers(groups) {
     log("Requesting users...");
     
-    const { stdout, stderr } = await exec(`aws iam list-users --query 'Users[].UserName' --output text`);
-    const users = stdout.split("\t");
+    const client = new IAMClient();
+    const input = { MaxItems: Infinity };
+    const command = new ListUsersCommand(input);
+    const { Users: users } = await client.send(command);
+
+    log(users);
 
     log("Requesting users [OK]");
     log("Requesting groups...");
 
     let userGroupsPromises = [];
     let done = 0;
-    
-    let indices = new Array(MAX_SHELL);
 
-
-    for (let user of users) {
-        const promise = fetch(`https://us-east-1.console.aws.amazon.com/iam/home?region=eu-west-1#/users/details/${user}?section=groups`);
-        userGroupsPromises.push(promise.then(({ stdout, stderr }) => {
-            progress(++done / users.length);
-            return { user, groups: stdout.split("\t") }
-        }));
-    }
+    // for (let user of users) {
+    //     const promise = fetch(`https://us-east-1.console.aws.amazon.com/iam/home?region=eu-west-1#/users/details/${user}?section=groups`);
+    //     userGroupsPromises.push(promise.then(({ stdout, stderr }) => {
+    //         progress(++done / users.length);
+    //         return { user, groups: stdout.split("\t") }
+    //     }));
+    // }
 
     const userGroups = await Promise.all(userGroupsPromises);
     log("Requesting groups [OK]");
 
 }
 
-const MAX_SHELL = 10;
-
-async function test() {
-    const client = new IAMClient();
-    const input = {};
-    const command = new ListUsersCommand(input);
-    const response = await client.send(command);
-    console.log(response);
-}
-
-test()
+getUsers();
